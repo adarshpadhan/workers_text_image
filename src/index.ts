3d911c3c5a14f99f246e1b100b32d7a69909fa0e
Adarsh Kumar Padhan <apadhan@outlook.in>
2025-02-13 12:53:39 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk cat",
+      prompt: "A University Classroom",
     };
 
     const response = await env.AI.run(
